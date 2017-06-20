f2a66d1c531c76c3336c0cb87c4372a9633ceb7e
ttmuangkeo <tim.muangkeo@gmail.com>
2017-06-20 16:44:45 -0700

    deleted swipe

--- plugins/script.js
+++ plugins/script.js
@@ -25,12 +25,4 @@ $(function() {
  wow.init();
 
 
- $('.carousel').swipe({
-     swipeLeft: function() {
-         $(this).carousel('next');
-     },
-     swipeRight: function() {
-         $(this).carousel('prev');
-     },
-     allowPageScroll: 'vertical'
- });
+
